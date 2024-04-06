13896460f3e48c8f4eae954cf05f5de828387c07
Mazire <mazirepooja666@gmail.com>
2024-04-06 23:20:16 +0530

    using vercel for backend deployment now

--- src/app/api.const.ts
+++ src/app/api.const.ts
@@ -1,3 +1,4 @@
-export const api_url =  'https://inventory-api-hful.onrender.com/api/v1/';
+export const api_url =  "https://inventory-api-pi.vercel.app/api/v1/";
+// 'https://inventory-api-hful.onrender.com/api/v1/';
 
 export const local_api_url = 'http://localhost:3000/api/v1/';
